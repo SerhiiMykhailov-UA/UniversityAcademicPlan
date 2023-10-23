7bf5f6fc26608ccfeb250895bc0e411efb62be2b
Serg <Serg@SergNout>
2023-10-23 21:30:18 +0300

    Fix boot setup for MVC

--- UniversityAcademicPlan/src/main/java/ua/foxminded/ServletInitializer.java
+++ UniversityAcademicPlan/src/main/java/ua/foxminded/ServletInitializer.java
@@ -1,16 +1,13 @@
 package ua.foxminded;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import ua.foxminded.universityacademicplan.UniversityAcademicPlan;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(UniversityAcademicPlan.class);
+		return application.sources(ServletInitializer.class);
 	}
 
 }
--- UniversityAcademicPlan/src/main/java/ua/foxminded/controller/FirstController.java
+++ UniversityAcademicPlan/src/main/java/ua/foxminded/controller/FirstController.java
@@ -3,6 +3,7 @@ package ua.foxminded.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +13,51 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
 	@GetMapping("/hello")
-	public String helloPage (HttpServletRequest request) {
+	public String helloPage (HttpServletRequest request, Model model) {
 		String name = request.getParameter("name");
 		String surname = request.getParameter("surname");
-		System.out.println("First " + name + " " + surname);
+//		System.out.println("First " + name + " " + surname);
+		model.addAttribute("message", "Hello " + name + " " + surname);
 		return "first/hello";
 	}
 	
 	@GetMapping("/goodbye")
-	public String goodbyePage (@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
+	public String goodbyePage (@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname, Model model) {
 		System.out.println("Second " + name + " " + surname);
+
+		model.addAttribute("message", "Goodbye " + name + " " + surname);
 		return "first/goodbye";
+	}
+	
+	@GetMapping("/calculator")
+	public String calculatorPage (@RequestParam (value = "a", required = false) int a,
+			@RequestParam(value = "b", required = false) int b,
+			@RequestParam(value = "action", required = false) String action, Model model) {
+		double c;
+		switch (action) {
+		case "multiplication":
+			action = " * ";
+			c = a * b;
+			break;
+		case "addition":
+			c = a + b;
+			action = " + ";
+			break;
+		case "substraction":
+			action = " - ";
+			c = a - b;
+			break;
+		case "division":
+			action = " / ";
+			c = a / (double)b;
+			break;
+
+		default:
+			c = 0;
+			break;
+		}
+		model.addAttribute("result", a + action + b + " = " + c);
+		return "first/calculator";
 	}
 }
